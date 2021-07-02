96fb8466017f48e02f54bc8abbe54fb5f2c9805b
sim young in <sspjyins@snu.ac.kr>
2021-07-02 13:20:58 +0900

    tls 1.2 multi echo server using openssl

---
+++ cmake-build-debug/CMakeFiles/Client1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Client1.dir/echo_client.c.o"
+  "Client1"
+  "Client1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Client1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Client2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Client2.dir/echo_client.c.o"
+  "Client2"
+  "Client2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Client2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
